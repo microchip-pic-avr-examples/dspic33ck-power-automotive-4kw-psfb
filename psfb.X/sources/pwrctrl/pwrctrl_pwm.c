a90f71339b032e978d5f442cca3173b220c7e3b0
Andreas Reiter - M91406 <andreas.reiter@microchip.com>
2024-12-17 13:54:02 +0100

    Moved PWM initialization from mcc_custom_extension to pwrctrl_pwm

--- psfb.X/sources/pwrctrl/pwrctrl_pwm.c
+++ psfb.X/sources/pwrctrl/pwrctrl_pwm.c
@@ -155,3 +155,95 @@ void PwrCtrl_PWM_Disable(void)
 //    PWM_SoftwareUpdateRequest(PWM_SEC_2);
     
 }
+
+void PwrCtrl_PWM_Initialize(void)
+{
+    /*
+     PG1 is the leading PWM channel
+     PG3 is triggered by PG1, where PG3 is synchronized to PG1 including updates
+     PG4 is sync rectifier of PWM1H+PWM3L and is also synchronized to PG1
+     PG2 is sync rectifier of PWM3H+PWM1L and is synchronized to PG3
+     All updates are triggered by the PG1 broadcast
+     All updates are performed at the cycle boundary (EOC)
+     PG2 and PG4 only use PWMxL output
+     PG2 and PG4 are swapped, hence, their respective HIGH signal is used
+     Sync Rectifier edge placement is tuned by PGxDTH and PGxDC of PG2 and PG4,
+          where the duty cycle = PER - PHASE-SHIFT +/- EDGEPLACEMENT 
+     */
+  
+    // Set base register of PWM channels
+    PG1CONL = 0x0088; // CLKSEL=1, HREN=1, ON=0
+    PG2CONL = 0x0088; // CLKSEL=1, HREN=1, ON=0
+    PG3CONL = 0x0088; // CLKSEL=1, HREN=1, ON=0
+    PG4CONL = 0x0088; // CLKSEL=1, HREN=1, ON=0
+    
+    // Set synchronization of PWM start and register update
+    PG1CONH = 0x0800; // SOCS=0, MSTEN=1
+    PG2CONH = 0x0243; // SOCS=3, TRGMOD=1, UPDMOD=2
+    PG3CONH = 0x0241; // SOCS=1, TRGMOD=1, UPDMOD=2
+    PG4CONH = 0x0241; // SOCS=1, TRGMOD=1, UPDMOD=2
+
+    // Set I/O Configuration
+    PG1IOCONL = 0x3000; // OVRENH/L=1
+    PG1IOCONH = 0;      // PENH/L=0
+    PG2IOCONL = 0x7000; // SWAP=1, OVRENH=1=1, OVRENL=1
+    PG2IOCONH = 0;      // PENH/L=0
+    PG3IOCONL = 0x3000; // OVRENH/L=1
+    PG3IOCONH = 0;      // PENH/L=0
+    PG4IOCONL = 0x7000; // SWAP=1, OVRENH=1=1, OVRENL=1
+    PG4IOCONH = 0;      // PENH/L=0
+    
+    // Set PG synchronization triggers
+    PG1EVTL = 0; // clear register
+    PG1EVTLbits.PGTRGSEL = 0b011; // use PGxTRIGC to set phase shift
+    PG1EVTLbits.UPDTRG = 0; // requires UPDREQ bit to be set manually
+    PG1EVTLbits.ADTR1EN1 = 1; // enable ADC trigger 1 => TRIGA
+    PG1EVTH = 0; // disable all triggers
+    PG1EVTHbits.ADTR2EN2 = 1; // enable ADC trigger 2 => TRIGB
+    
+    PG2EVTL = 0; // disable all triggers
+    PG2EVTH = 0; // disable all triggers
+    PG3EVTLbits.PGTRGSEL = 0b011; // use PGxTRIGC to set phase shift
+    PG3EVTLbits.UPDTRG = 0; // requires UPDREQ bit to be set manually
+    PG3EVTH = 0; // disable all triggers
+    PG4EVTL = 0; // disable all triggers
+    PG4EVTH = 0; // disable all triggers
+    
+    // Set Period
+    PG1PER = 40000;
+    PG2PER = 40000;
+    PG3PER = 40000;
+    PG4PER = 40000;
+
+    // Set Duty Cycle
+    PG1DC = 20000;
+    PG3DC = 20000;
+    
+    // Set dead times
+    PG1DTH = 640;   // set dead time to 160ns
+    PG1DTL = 640;   // set dead time to 160ns
+    PG2DTH = 0;     // no dead time on PWMxH
+    PG2DTL = 640;  // set dead time to 160ns
+    PG3DTH = 640;   // set dead time to 160ns
+    PG3DTL = 640;   // set dead time to 160ns
+    PG4DTH = 0;     // no dead time on PWMxH
+    PG4DTL = 640;  // set dead time to 160ns
+
+    // Set synchronization triggers
+    PG1TRIGC = 0; // phase shift = 0
+    PG3TRIGC = PG1DC; // phase shift = duty cycle of leading PWM channel
+    PG2DC = (PG1PER - PG1TRIGC - PG2DTL);
+    PG4DC = (PG1PER - PG1TRIGC - PG4DTL);
+    
+    // Configure the PWM value distribution data structure
+    PhaseShiftDistribution.PhaseShift = 0;  // Clear output value of phase shift
+    PhaseShiftDistribution.ptrPhaseShift = &PG1TRIGC;
+    PhaseShiftDistribution.ptrPeriod = &PG1PER;
+    PhaseShiftDistribution.ptrDCSRL = &PG4DC;
+    PhaseShiftDistribution.ptrDCSRR = &PG2DC;
+    PhaseShiftDistribution.ptrUpdateReg = &PG1STAT;
+    
+}
+
+
+
